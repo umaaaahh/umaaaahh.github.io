759afda0091f4135ade3309c0c80110c3efd8b6c
umaaaahh <s4080299@student.rmit.edu.au>
2025-06-04 16:10:26 +1000

    Final with a few bug fixes

--- interactivemedia/assignment3/script.js
+++ interactivemedia/assignment3/script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     girl2: document.querySelector(".girl2"),
     boy: document.querySelector(".boy"),
     lovers: document.querySelector(".lovers"),
-    angel: document.querySelector (".angel")
+    angel: document.querySelector (".angel"),
+    angel2: document.querySelector (".angel2"),
+    girl3: document.querySelector(".girl3")
+    gust: document.querySelector(".gust")
   };
 
   const stage = document.querySelector(".stage");
@@ -58,8 +61,8 @@ document.addEventListener("DOMContentLoaded", () => {
             characters.boy.classList.add("walk-in");
             stage.classList.remove("hide-background"); // Restore background
           } else {
-          characters.boy.classList.remove("walk-in");
-          // characters.girl.classList.remove("visible");
+          characters.boy.classList.remove("walk-in", "visible");
+          
           }
           break;
 
@@ -74,15 +77,20 @@ document.addEventListener("DOMContentLoaded", () => {
             toggleVisibility(characters.lovers, visible);
 
             if (visible) {
-              heavenClouds.classList.add("visible");
+                heavenClouds.classList.add("visible");
+
+                // Ensure lovers shrink in stanza 5
+                characters.lovers.classList.add("shrink"); 
             } else {
-              heavenClouds.classList.remove("visible"); // Reset if scrolling back up
+                heavenClouds.classList.remove("visible"); 
+                characters.lovers.classList.remove("shrink"); // Reset shrink when scrolling back up
             }
             break;
 
 
-          case 6: {
+          case 6: 
             toggleVisibility(characters.angel, visible); 
+            toggleVisibility(characters.gust, visible);
               if (visible) {
                 characters.angel.classList.add("visible");
                 stage.classList.remove("hide-background");
@@ -92,7 +100,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
           
             break;
-          }
+            
+          case 7: 
+            toggleVisibility(characters.girl3, visible);
+            toggleVisibility(characters.angel2, visible);
+            if (visible) {
+              characters.girl3.classlist.add("visible")
+              characters.angel2.classlist.add("visible")
+            } else {
+              characters.girl3.classlist.remove("visible")
+              characters.angel2.classList.remove("visible");
+            }
+          
 
         default:
           // If any stanza before 4 is intersecting, ensure background is visible
